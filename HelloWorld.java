c2646c544cd2fc88d33ff4190344f32ef1d06c43
kankan-k2 <kankankk@gmail.com>
2021-07-14 10:28:03 +0530

    first update

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
         System.out.println("hello World");
 
+        System.out.println("Printing 1 to 10");
+
         for (int i = 1; i <= 10; i++){
             System.out.println(i);
         }
